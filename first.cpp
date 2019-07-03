f29cb9080871e77f8f277493294207c2a9a61057
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2019-07-02 21:28:23 -0400

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,25 @@
+/*
+ID: vanshse1
+TASK: test
+LANG: C++                 
+*/
+
+/* 
+By. Vansh Sethi
+MAIN CONCEPT = Ad Hoc
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
+}
